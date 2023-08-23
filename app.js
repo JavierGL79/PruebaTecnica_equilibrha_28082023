6e3deb234be89976dc350ea4d7deb0fc01f44d6b
Javier Giron Lopez <javiergironlopezbpr@gmail.com>
2023-08-23 14:23:20 +0200

    Women and Men counter OK

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const xlsx = require('xlsx')
 const path = 'datos_prueba_tecnica.xlsx'
 const finalText = 'END OF SCRIPT'
-
+const dataExcel = (path) => readingExcelFile(path)
 //Function that reads and saving data in json format from an xlsx file.
 const readingExcelFile = (path) => {
     try {
@@ -16,18 +16,48 @@ const readingExcelFile = (path) => {
     }
 }
 
-
-//Script
-console.log()
-console.log('Technical testing')
-console.log('... ...')
-if (readingExcelFile(path) != null) {
-    console.log('Successfully saving data in dataExcel')
-    console.log()
-} else {
-    console.log()
-    console.log(finalText)
+//Function showing men and women of the total number of employees
+const sexCounter = (dataExcel) => {
+    let employeesCounter = 0;
+    let sexH = 0;
+    let sexM = 0;
+    try {
+        for (let sexCounter of dataExcel){
+            employeesCounter++;
+            if(sexCounter['sexo'] == 'M'){
+                sexM++;
+            }else if(sexCounter['sexo'] == 'H'){
+                sexH++;
+            }
+        }
+        console.log(`There are ${sexM} women & ${sexH} men of a total of ${employeesCounter} employees`);
+        
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
-console.log(`${finalText}`)
-console.log('(by Javier Girón López)')
+//Script
+console.log();
+console.log('Technical testing');
+console.log('... ...');
+if (readingExcelFile(path) != null) {
+    console.log('Successfully saving data in dataExcel');
+    console.log();
+}if(readingExcelFile(path) != null){
+    console.log('1.- How many men and women are there of the total number of employees')
+    sexCounter(readingExcelFile(path))
+    console.log();
+}if(readingExcelFile(path) != null){
+    console.log('1.- How many men and women are there of the total number of employees')
+    grossSalary(readingExcelFile(path))
+    console.log();
+}
+else {
+    console.log();
+    console.log(finalText);
+};
+
+console.log(`${finalText}`);
+console.log('(by Javier Girón López)');
